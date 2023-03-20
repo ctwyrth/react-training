ddefc7f08e8efad061af3d93c9c51cd78bdb26cc
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-03-20 12:25:05 -0700

    3rd, adding button function to open/close alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 // import ListGroup from './components/ListGroup';
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 export default function App() {
-  // let cities = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-  // let colors = ['blue', 'red', 'black', 'purple', 'yellow'];
+  const alertPlaceholder = document.getElementById('myAlertPlaceholder');
 
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // }
+  const alert = (message: string, type: string) => {
+    const wrapper = document.createElement('div');
+
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      ` <div>${message}</div>`,
+      ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('');
+
+    alertPlaceholder?.append(wrapper);
+  }
 
   return (
-    <div className="container-fluid">
-      <Alert>
-        Hello <em>World!</em>
-      </Alert>
+    <div className="container-fluid mt-3">
+      <div id="myAlertPlaceholder"></div>
+      <Button color="danger" onClick={() => { 
+        alert("What the hell have you done?", "danger");
+        console.log('click');
+      }}>Crazy</Button>
+      <Button onClick={() => alert("I mean, was this really necessary?", "secondary")}>Crazy</Button>
+      <Button color="success" onClick={() => alert("Would you ever have expected this to work?", "success")}>Crazy</Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children: string;
+  color?: 'primary' | 'secondary' | 'success' | 'danger';
+  onClick: () => void;
+}
+
+const Button = ({ children, color = 'secondary', onClick }: Props) => {
+  return (
+    <button type="button" id="myAlert" className={`btn btn-${color}`} onClick={onClick}>{children}</button>
+  )
+}
+
+export default Button;
